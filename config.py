0481edb4801d108d03d7ceb421fc4044d4aa5586
GodFZ <45254831+GodFZ@users.noreply.github.com>
2026-01-13 19:52:03 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+
+cub_root = ''
+car_root = ''
+flower_root = ''
+imagenet_root = ''
+
+# OSR Split dir
+osr_split_dir = 'data/ssb_splits'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+exp_root = 'dev_outputs' # All logs and checkpoints will be saved here
